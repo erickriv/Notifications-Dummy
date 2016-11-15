f771b8a0f9d0704a4bbbd7adf2806c58d8644de2
krystalNatalia <krystal.medina1@upr.edu>
2016-11-15 17:09:11 -0400

    Switchesssss niggaaaa

--- notification_switchTableViewController.swift
+++ notification_switchTableViewController.swift
@@ -12,13 +12,49 @@ class notification_switchTableViewController: UITableViewController {
     
     @IBOutlet var mySwitch: UISwitch?
     
+    let switches = ["CAM", "Administración de Empresas", "Biología", "Ciencias de Cómputos", "Ciencias Sociales",
+        "Comunicación Tele-Radial", "Educación", "Enfermería", "Español", "Física-Química", "Humanidades",
+        "Inglés", "Matemáticas", "Sistemas de Oficina"]
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        mySwitch?.addTarget(self, action: #selector(notification_switchTableViewController.switchIsChanged(_:)), forControlEvents: UIControlEvents.ValueChanged)
+        
+        //mySwitch?.addTarget(self, action: #selector(notification_switchTableViewController.switchIsChanged(_:)), forControlEvents: UIControlEvents.ValueChanged)
     }
 
+    override func didReceiveMemoryWarning() {
+        
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+    override func tableView (_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return switches.count
+    }
+    
+    override func tableView (_ tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        
+        cell.textLabel!.text = switches[indexPath.row]
+        
+        // sets font style and size to respective devices (phone or pad)
+        
+        if (UIDevice.currentDevice().userInterfaceIdiom == UIUserInterfaceIdiom.Phone) {
+            cell.textLabel!.font = UIFont(name: "Arial", size: 19)
+        }
+        
+        if (UIDevice.currentDevice().userInterfaceIdiom == UIUserInterfaceIdiom.Pad) {
+            cell.textLabel!.font = UIFont(name: "Arial", size: 25)
+        }
+        return cell
+        
+    }
 
     @IBAction func switchIsChanged(mySwitch: UISwitch) {
+        
         if mySwitch.on {
             print("UISwitch is ON")
         } else {
